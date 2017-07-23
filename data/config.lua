7e9225554b83daaf6c6c983a058309862557e9d4
MrLock1378 <rezafaghihi1379@gmail.com>
2017-07-23 19:04:40 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,44 @@
+do local _ = {
+  admins = {
+    {
+      331486503,
+      "@Sudo\\_balck"
+    },
+    {
+      335856639,
+      "@cfz\\_Hamed\\_cfz"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun",
+    "addkick",
+    "del",
+    "id",
+    "Bokonesh",
+    "Support",
+    "offline",
+    "share",
+    "stats",
+    "Like",
+    "gif",
+    "00",
+    "sticker"
+  },
+  info_text = "Ad̈m̈ïn̈s̈: @SudoLocker\nCḧän̈n̈ël̈: @LockerTeam\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    374615287,
+    374734484,
+    374734484
+  }
+}
+return _
+end
